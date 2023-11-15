454d2606a74e63af1cf8cf3bf8084e6da89e9178
Felipe Martins Bittencourt <felipebittencourt@id.uff.br>
2023-11-15 00:38:14 -0300

    feat: coletando nome e ultima mensagem do cliente

--- chatBot.py
+++ chatBot.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+
 from selenium.webdriver.common.keys import Keys
 import time
 from selenium.webdriver.common.by import By
@@ -6,6 +7,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 print("It's running")
+# Abrindo o navegador e redirecionando pro whatsapp web
 navegador = webdriver.Chrome()
 navegador.get("https://web.whatsapp.com/")
 
@@ -15,11 +17,25 @@ while len(navegador.find_elements(By.ID, "side")) < 1:
 time.sleep(5)
 while True:
     try:
-        # Tente encontrar uma conversa não lida
+
+        # Tentando encontrar uma conversa não lida e entrar nela
         conversaNaoLida = WebDriverWait(navegador, 5).until(EC.presence_of_element_located(
             (By.CLASS_NAME, "l7jjieqr.cfzgl7ar.ei5e7seu.h0viaqh7.tpmajp1w.c0uhu3dl.riy2oczp.dsh4tgtl.sy6s5v3r.gz7w46tb.lyutrhe2.qfejxiq4.fewfhwl7.ovhn1urg.ap18qm3b.ikwl5qvt.j90th5db.aumms1qt")))
         conversaNaoLida.click()
+        
+        # Obtenha o nome do cliente
+        nomeCliente = WebDriverWait(navegador, 5).until(EC.presence_of_element_located(
+            (By.XPATH, '//*[@id="main"]/header/div[2]/div/div/div/span')))
+        nome = nomeCliente.text
 
+        # Obtenha o conteúdo da última mensagem do cliente
+
+        todasMensagens = WebDriverWait(navegador, 5).until(
+            EC.presence_of_all_elements_located((By.CLASS_NAME, "_21Ahp")))
+        textosMensagens = [mensagem.text for mensagem in todasMensagens]
+        textoUltimaMensagem = textosMensagens[-1]
+        print(textoUltimaMensagem)
+        
     except:
         time.sleep(1)
         continue
