02cd13cbb47698a811b0f5aaf8cecee1361550bb
Felipe Martins Bittencourt <felipebittencourt@id.uff.br>
2023-11-15 00:31:00 -0300

    feat: entrando no chrome e redirecionando para o wpp

--- chatBot.py
+++ chatBot.py
@@ -5,3 +5,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+print("It's running")
+navegador = webdriver.Chrome()
+navegador.get("https://web.whatsapp.com/")
