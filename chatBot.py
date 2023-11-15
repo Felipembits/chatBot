fa98536543bc7f97f9984eeb0cad2e5e45b340b1
Felipe Martins Bittencourt <felipebittencourt@id.uff.br>
2023-11-15 00:32:47 -0300

    feat: aguardando o login no whatsapp

--- chatBot.py
+++ chatBot.py
@@ -8,3 +8,8 @@ from selenium.webdriver.support import expected_conditions as EC
 print("It's running")
 navegador = webdriver.Chrome()
 navegador.get("https://web.whatsapp.com/")
+
+while len(navegador.find_elements(By.ID, "side")) < 1:
+    time.sleep(1)
+
+time.sleep(5)
