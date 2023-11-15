1fd25e3409dfa5f21b8d9e1bc49d1d817c66428f
Felipe Martins Bittencourt <felipebittencourt@id.uff.br>
2023-11-15 00:34:01 -0300

    feat: entrando na conversa não lida

--- chatBot.py
+++ chatBot.py
@@ -13,3 +13,13 @@ while len(navegador.find_elements(By.ID, "side")) < 1:
     time.sleep(1)
 
 time.sleep(5)
+while True:
+    try:
+        # Tente encontrar uma conversa não lida
+        conversaNaoLida = WebDriverWait(navegador, 5).until(EC.presence_of_element_located(
+            (By.CLASS_NAME, "l7jjieqr.cfzgl7ar.ei5e7seu.h0viaqh7.tpmajp1w.c0uhu3dl.riy2oczp.dsh4tgtl.sy6s5v3r.gz7w46tb.lyutrhe2.qfejxiq4.fewfhwl7.ovhn1urg.ap18qm3b.ikwl5qvt.j90th5db.aumms1qt")))
+        conversaNaoLida.click()
+
+    except:
+        time.sleep(1)
+        continue
